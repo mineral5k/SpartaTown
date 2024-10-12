438cfff01dcc63d1811a0c6ccdeb40f3e82db753
mineral5k <rkskek169@gmail.com>
2024-10-13 03:45:24 +0900

    22

--- Assets/Scripts/JoinButtonMS.cs
+++ Assets/Scripts/JoinButtonMS.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class JoinButton : MonoBehaviour
+public class JoinButtonMS : MonoBehaviour
 {
     [SerializeField] public InputField inputField;
+    [SerializeField] public GameObject panel;
     // Start is called before the first frame update
-
-    private void Awake()
+    void Start()
     {
         
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
     public void Enter()
     {
         GameManager.Instance.playerName = inputField.text;
-       
-        SceneManager.LoadScene("MainScene");
-        
+        panel.SetActive(false);
+
+
     }
 }
